afa169932314dda755ffbfa64d2099ab075dbf76
WuSKS <145275222+WuSKS@users.noreply.github.com>
2023-10-17 16:52:04 +0800

    Add files via upload

---
+++ Teachers.java
@@ -0,0 +1,27 @@
+package XuanKe;
+
+public class Teachers extends Students {
+	public Teachers(int num,String name,String gender,Course course){
+		super(num,name,gender);
+		this.course=course;}
+		
+		
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+	}
+	
+	
+
